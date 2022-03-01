b7216c26fac19507402a65f9b03d5dbc92dff6d2
Ayman Salah <ayman.1551.salah@gmail.com>
2022-03-01 17:45:38 +0200

    Made the posts app

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
 
 # Create your views here.
+class HomePageView(ListView):
+    model = Post
+    template_name = 'posts/home.html'
+    # Notice that , HOmePageView will Inherit from List View
+    #  And Hence it will Return Object_list
+    # Which is The Context
+    # context_object_name =  We will test this Vairalbe later  ;
+    context_object_name = 'The_sent_context'
